fa56874d920a35a9cd477cdbf998f68bd4eadab7
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-03-15 20:43:24 +0100

    pre-commit configuration for black and flake8
    settigns
    
    fix

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-q9cxx(_9q$u@hbswrl!mbjcq=uxo93(q6=o(jd$tr+u&03zcqa"
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
