6bf87baf88225d56bca96615417fb12b88e129e3
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-03-27 17:20:10 +0200

    reformatted files

--- config/settings.py
+++ config/settings.py
@@ -15,19 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-q9cxx(_9q$u@hbswrl!mbjcq=uxo93(q6=o(jd$tr+u&03zcqa"
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -38,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "products.apps.ProductsConfig",
     "users.apps.UsersConfig",
 ]
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -113,7 +108,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -123,3 +117,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.CustomUser"
--- users/models.py
+++ users/models.py
@@ -5,15 +5,4 @@ from django.db import models
 class CustomUser(AbstractUser):
     """Custom User placeholder."""
 
-    shoe_number = models.FloatField()
-
-    def __str__(self) -> str:
-        return str(self.shoe_number)
-
-    def get_canonical_url(self) -> str:
-        """Generate Canonical URL for Custom User.
-
-        :returns: canonical URL
-        :rtype: str
-        """
-        return f"/{self.shoe_number}/canonical/"
+    pass
