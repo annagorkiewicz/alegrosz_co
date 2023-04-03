99746a50120e6dc77149f98d1aea2d155e5b0d10
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-04-03 19:37:08 +0200

    paginator tests

--- products/tests/test_viewsets.py
+++ products/tests/test_viewsets.py
@@ -2,7 +2,7 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIRequestFactory
 
-from conftest import ProductFactory
+from conftest import ProductFactory, api_rf
 from products.models import Product
 from products.viewsets import ProductViewSet
 
@@ -56,7 +56,47 @@ def test_all_products_viewsets_with_empty_db(db, api_rf):
     assert len(response.data.get("results")) == 0
 
 
-def test_product_factory(products_batch):
-    products = Product.objects.all()
+def test_product_factory(products_batch, api_rf):
+    url = "/api/v1/products/"
+    view = ProductViewSet.as_view({"get": "list"})
 
-    assert products.count() == 60
+    request = api_rf.get(url)
+    response = view(request)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data.get("count") == 60
+    assert "page=2" in response.data.get("next")
+    assert response.data.get("previous") is None
+    assert len(response.data.get("results")) == 20
+
+
+def test_product_pagination_last_page(products_batch, api_rf):
+    url = "/api/v1/products/?page=3"
+    view = ProductViewSet.as_view({"get": "list"})
+
+    request = api_rf.get(url)
+    response = view(request, query_params=3)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data.get("count") == 60
+    assert response.data.get("next") is None
+    assert "page=2" in response.data.get("previous")
+    assert len(response.data.get("results")) == 20
+
+
+def test_product_list_without_pagination(products_batch, api_rf):
+    url = "/api/v1/products/"
+    ProductViewSet.pagination_class = None
+    view = ProductViewSet.as_view({"get": "list"})
+
+    request = api_rf.get(url)
+    response = view(request)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 60
+
+
+@pytest.mark.skip("WIP")
+def test_product_pagination_ordering():
+    pass
+
